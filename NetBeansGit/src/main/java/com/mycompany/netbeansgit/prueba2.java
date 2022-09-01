37634c3d61a1c91e952a6381234fe17d40be96d5
Leandro Molina <leandrorodrigomolina@gmail.com>
2022-09-01 11:02:13 -0300

    se agrego prueba2

---
+++ NetBeansGit/src/main/java/com/mycompany/netbeansgit/prueba2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.netbeansgit;
+
+/**
+ *
+ * @author pepeponpin
+ */
+public class prueba2 {
+    private int dato1;
+    private float dato2;
+
+    public prueba2(int dato1, float dato2) {
+        this.dato1 = dato1;
+        this.dato2 = dato2;
+    }
+
+    public int getDato1() {
+        return dato1;
+    }
+
+    public void setDato1(int dato1) {
+        this.dato1 = dato1;
+    }
+
+    public float getDato2() {
+        return dato2;
+    }
+
+    public void setDato2(float dato2) {
+        this.dato2 = dato2;
+    }
+    
+}
